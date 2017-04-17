c9c1bbd0f0bb559202edf0007042fe2cab0787b9
Shawn <tue53519@temple.edu>
2017-04-17 03:21:18 -0400

    Added working list

--- app/app.py
+++ app/app.py
@@ -1,26 +1,74 @@
 from sanic import Sanic
 from sanic import response
 
+from sanic_session import InMemorySessionInterface
+from sanic_jinja2 import SanicJinja2
+
 import aiohttp
 
-app = Sanic(__name__)
+app = Sanic()
 
-async def fetch(session, url):
+jinja = SanicJinja2(app)
+session = InMemorySessionInterface(cookie_name=app.name, prefix=app.name)
+
+head = """<!DOCTYPE html>
+<html>
+<head>
+<style>
+table, th, td {
+    border: 1px solid black;
+}
+</style>
+</head>
+<body>
+<table>
+"""
+
+footer = """</table>
+</body>
+</html>"""
+
+
+
+@app.middleware('request')
+async def add_session_to_request(request):
+    # before each request initialize a session
+    # using the client's request
+    await session.open(request)
+
+
+@app.middleware('response')
+async def save_session(request, response):
+    # after each request save the session,
+    # pass the response to set client cookies
+    await session.save(request, response)
+
+async def fetch(get_session, url):
     """
     Use session object to perform 'get' request on url
     """
-    async with session.get(url) as result:
+    async with get_session.get(url) as result:
         return await result.json()
 
 
-@app.route('/')
-async def handle_request(request):
-    url = "https://api.github.com/repos/channelcat/sanic"
-    
-    async with aiohttp.ClientSession() as session:
-        result = await fetch(session, url)
-        return response.json(result)
+@app.route('/<user>/<repo>')
+async def handle_request(request, user, repo):
+        url = "https://api.github.com/repos/" + user + "/" + repo + ""
+        
+        async with aiohttp.ClientSession() as get_session:
+               	result = await fetch(get_session, url)
+                page = str(head)
+                for key, value in result.items():
+                        table = """<tr>
+                                    <td>""" + str(key) + """</td>
+                                    <td>""" + str(value) + """</td>
+                                   </tr>"""
+                        page += table
+                page += footer
 
+
+                return response.html(page)
+                #return jinja.render('index.html', request, args=None)
 
 if __name__ == '__main__':
 	app.run(host="0.0.0.0", port=8000, workers=2)
